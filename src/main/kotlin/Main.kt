1e452db74cf8f68e560998d5f45fb1e0083a09dc
HRJuan580 <jherrel580@g.educaand.es>
2025-05-08 20:01:28 +0200

    commit 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,36 @@
 package iesra.dawb.seguros
 
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
 fun main() {
-    println("Hello World!")
+    val jdbcUrl = "jdbc:h2:./data/tienda"
+    val user = "usuario"
+    val password = ""
+
+    var connection: Connection? = null
+
+    try {
+
+        connection = DriverManager.getConnection(jdbcUrl, user, password)
+        println("Conexión exitosa a la base de datos H2.")
+
+        val statement = connection.createStatement()
+        val resultSet = statement.executeQuery("SELECT 1+1 AS Resultado")
+        while (resultSet.next()) {
+            println("Resultado de prueba: ${resultSet.getInt("Resultado")}")
+        }
+
+    } catch (e: SQLException) {
+        println("Error al conectar con la base de datos: ${e.message}")
+    } finally {
+
+        try {
+            connection?.close()
+            println("Conexión cerrada.")
+        } catch (e: SQLException) {
+            println("Error al cerrar la conexión: ${e.message}")
+        }
+    }
 }
