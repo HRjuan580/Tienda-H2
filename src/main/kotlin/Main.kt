a2fee5f6c1c2e8c0470c90e5ec9148988e30e244
HRJuan580 <jherrel580@g.educaand.es>
2025-05-08 20:33:43 +0200

    Codigo Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,32 +5,31 @@ import java.sql.DriverManager
 import java.sql.SQLException
 
 fun main() {
-    val jdbcUrl = "jdbc:h2:./data/tienda"
-    val user = "usuario"
-    val password = ""
+    val jdbcUrl = "jdbc:h2:./data/tienda"  
+    val user = "sa"                        
+    val password = ""                      
 
     var connection: Connection? = null
 
     try {
-
         connection = DriverManager.getConnection(jdbcUrl, user, password)
-        println("Conexión exitosa a la base de datos H2.")
+        println(" Conexión exitosa a la base de datos H2.")
 
         val statement = connection.createStatement()
-        val resultSet = statement.executeQuery("SELECT 1+1 AS Resultado")
+        val resultSet = statement.executeQuery("SELECT 1 + 1 AS Resultado")
         while (resultSet.next()) {
             println("Resultado de prueba: ${resultSet.getInt("Resultado")}")
         }
 
     } catch (e: SQLException) {
-        println("Error al conectar con la base de datos: ${e.message}")
+        println(" Error al conectar con la base de datos: ${e.message}")
     } finally {
-
         try {
             connection?.close()
-            println("Conexión cerrada.")
+            println(" Conexión cerrada.")
         } catch (e: SQLException) {
-            println("Error al cerrar la conexión: ${e.message}")
+            println(" Error al cerrar la conexión: ${e.message}")
         }
     }
 }
+
